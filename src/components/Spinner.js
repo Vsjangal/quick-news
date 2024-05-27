1eb8cbbb18412b5f430248b4893ba92bb9752598
Vijay Jangal <jangalvijay36@gmail.com>
2024-05-27 19:19:38 +0530

    fixed styling

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import loader from './loader.gif'
+import React from "react";
+import loader from "./loader.gif";
 
 const Spinner = () => {
-    return (
-        <div className='text-center'>
-            <img className="my-3" src={loader} alt="loading" />
-        </div>
-    )
+  return (
+    <div className="text-center">
+      <img
+        className="my-3"
+        src={loader}
+        alt="loading"
+      />
+    </div>
+  );
+};
 
-}
-
-export default Spinner
+export default Spinner;
