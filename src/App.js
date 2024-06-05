bd269da0ed4dc4560c6620a226b89a5cbeeac5cd
Vijay Jangal <jangalvijay36@gmail.com>
2024-06-06 04:41:51 +0530

    fixed all the issues

--- src/App.js
+++ src/App.js
@@ -1,52 +1,36 @@
-import './App.css';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import NavBar from "./components/NavBar/NavBar";
+import News from "./components/News/News";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { router } from "./config/config";
+import Search from "./components/Search/Search";
 
-import React, { useState } from 'react'
-import NavBar from './components/NavBar';
-import NewsBox from './components/NewsBox';
-import { HashRouter, Routes, Route } from "react-router-dom";
-import LoadingBar from 'react-top-loading-bar'
-
-const App = () => {
-  const pageSize = 6;
-  const apiKey = process.env.REACT_APP_NEWS_API;
-  // console.log(apiKey)
-  // const apiKey = "e523fb7bc0384bcc89c049aa484bbb43";
-
-  const [mode, setMode] = useState('light');
-  const [progress, setProgress] = useState(0)
-  const toggleMode = () => {
-    if (mode === 'light') {
-      setMode('dark')
-      document.body.style.backgroundColor = '#282828';
-    }
-    else {
-      setMode('light')
-      document.body.style.backgroundColor = 'white'
-    }
-  }
-
+function App() {
   return (
-    <HashRouter>
-      <div>
-        <LoadingBar
-          color='#f11946'
-          height={3}
-          progress={progress}
-        />
-        <NavBar mode={mode} toggleMode={toggleMode} />
+    <>
+      <Router>
+        <NavBar />
         <Routes>
-          <Route path='/' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='general' pageSize={pageSize} country="in" category="" />}></Route>
-          <Route path='/business' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='business' pageSize={pageSize} country="in" category="business" />}></Route>
-          <Route path='/entertainment' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='entertainment' pageSize={pageSize} country="in" category="entertainment" />}></Route>
-          <Route path='/general' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='general' pageSize={pageSize} country="in" category="general" />}></Route>
-          <Route path='/health' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='health' pageSize={pageSize} country="in" category="health" />}></Route>
-          <Route path='/science' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='science' pageSize={pageSize} country="in" category="science" />}></Route>
-          <Route path='/sports' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='sports' pageSize={pageSize} country="in" category="sports" />}></Route>
-          <Route path='/technology' element={<NewsBox mode={mode} toggleMode={toggleMode} setProgress={setProgress} apiKey={apiKey} key='technology' pageSize={pageSize} country="in" category="technology" />}></Route>
+          {router.map((path) => (
+            <Route
+              exact
+              key={uuidv4()}
+              path={path.path}
+              element={
+                <News
+                  key={path.key}
+                  newscategory={path.category}
+                  country={path.country}
+                />
+              }
+            />
+          ))}
+          <Route path="/search/:query" element={<Search />} />
         </Routes>
-      </div>
-    </HashRouter>
-  )
-
+      </Router>
+    </>
+  );
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = document.getElementById("root");
+const rootElement = createRoot(root);
+
+rootElement.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
