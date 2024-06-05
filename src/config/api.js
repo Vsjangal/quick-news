dc64e8e5709ef3a6c4252555ff8b8ae7b64db7d6
Vijay Jangal <jangalvijay36@gmail.com>
2024-06-06 04:28:08 +0530

    added config and redux store

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
+const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
+const API_KEY = "5f063c0c6185787e0398225173fcd125";
+export const endpointPath = (country, category) =>
+  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+export const endpointSearch = (searchQuery) =>
+  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
